6a79f37ce820ceb0f08929cdc75b17a572249e26
SebastianMirandaAhumada <se.mirandaa@duocuc.cl>
2024-11-16 08:39:29 -0300

    se agregaron nuevas vistas

---
+++ src/main/java/com/example/seguridad_calidad/controller/RecetasCompartidas.java
@@ -0,0 +1,12 @@
+package com.example.seguridad_calidad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RecetasCompartidas {
+      @GetMapping("/recetas-compartidas")
+    public String showUpload() {
+        return "recetas-compartidas";
+    }
+}
